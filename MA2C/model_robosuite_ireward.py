c3d3a84a2f9347a20dc5a6999e65bdb92d30975a
tlaw009 <sxl961@miami.edu>
2022-06-15 18:24:45 -0400

    ir render

--- MA2C/model_robosuite_ireward.py
+++ MA2C/model_robosuite_ireward.py
@@ -53,7 +53,7 @@ env = suite.make(
     env_name="Lift", # try with other tasks like "Stack" and "Door"
     robots="Panda",  # try with other robots like "Sawyer" and "Jaco"
     controller_configs=config,
-    has_renderer=False,
+    has_renderer=True,
     ignore_done=False,
     has_offscreen_renderer=False,
     reward_shaping=True,
@@ -419,7 +419,7 @@ while ep < total_episodes:
         while True:
             # Uncomment this to see the Actor in action
             # But not in a python notebook.
-            # env.render()
+            env.render()
 
             tf_prev_state = tf.expand_dims(tf.convert_to_tensor(prev_state), 0)
 
@@ -490,7 +490,7 @@ while ep < total_episodes:
         while True:
             # Uncomment this to see the Actor in action
             # But not in a python notebook.
-            # env.render()
+            env.render()
 
             tf_prev_state = tf.expand_dims(tf.convert_to_tensor(prev_state), 0)
 
