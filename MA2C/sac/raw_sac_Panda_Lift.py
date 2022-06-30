44a2703f2125981f03079295663ac85bc476d216
tlaw009 <sxl961@miami.edu>
2022-06-30 15:58:38 -0400

    reorg run file

--- MA2C/sac/raw_sac_Panda_Lift.py
+++ MA2C/sac/raw_sac_Panda_Lift.py
@@ -269,7 +269,7 @@ class Actor(Model):
             log_pi = tf.expand_dims(log_pi_ - tf.reduce_sum(tf.math.log(1 - action**2 + EPSILON), axis=1),
                                         -1)        
 
-        return action*upper_bound, log_pi
+        return action, log_pi
 
 def get_critic():
     # State as input
@@ -369,7 +369,7 @@ while t_steps < 1000000:
         action = action[0]
 
         # Recieve state and reward from environment.
-        state, reward, done, info = env.step(action)
+        state, reward, done, info = env.step(action*upper_bound)
 
         state_reshaped = []
 
@@ -415,7 +415,7 @@ while t_steps < 1000000:
                 eval_action = eval_action[0]
 
                 # Recieve state and reward from environment.
-                eval_state, eval_reward, eval_done, info = eval_env.step(eval_action)
+                eval_state, eval_reward, eval_done, info = eval_env.step(eval_action*upper_bound)
 
                 eval_state_reshaped = []
 
