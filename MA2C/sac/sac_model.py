4f1a8fe57097edb393a2068dca25b198cd48d28a
tlaw009 <sxl961@miami.edu>
2022-07-31 18:21:57 -0400

    test set 2

--- MA2C/sac/sac_model.py
+++ MA2C/sac/sac_model.py
@@ -37,8 +37,27 @@ print("Min Value of Action ->  {}".format(lower_bound), flush=True)
 
 #################### Auxiliaries ####################
 
-##########*****####################*****##########
+###########################
+#Observation normalization#
+###########################
 
+obs_upper = np.zeros(num_states)
+obs_lower = np.zeros(num_states)
+
+def obs_norm(state):
+    norm_state = np.zeros(num_states)
+    for i in range(num_states):
+        if state[i] > obs_upper[i]:
+            obs_upper[i] = state[i]
+        if state[i] < obs_lower[i]:
+            obs_lower[i] = state[i]
+        norm_state[i] = state[i]/(obs_upper[i] - obs_lower[i]+EPSILON)
+
+    return norm_state
+
+print("State Normalization Initialized", flush=True)
+
+##########*****####################*****##########
 
 #################### Replay Buffer ####################
 
@@ -81,13 +100,13 @@ class Buffer:
         # Training and updating Actor & Critic networks.
         with tf.GradientTape() as tape1:
             # Get Q value estimates, action used here is from the replay buffer
-            q1 = critic_model_1([state_batch, action_batch], training=True)
+            q1 = critic_model_1([state_batch, action_batch])
             # Sample actions from the policy for next states
             pi_a, log_pi_a = actor_model(next_state_batch)
 
             # Get Q value estimates from target Q network
-            q1_target = target_critic_1([next_state_batch, pi_a], training=True)
-            q2_target = target_critic_2([next_state_batch, pi_a], training=True)
+            q1_target = target_critic_1([next_state_batch, pi_a])
+            q2_target = target_critic_2([next_state_batch, pi_a])
 
             # Apply the clipped double Q trick
             # Get the minimum Q value of the 2 target networks
@@ -103,13 +122,13 @@ class Buffer:
 
         with tf.GradientTape() as tape2:
             # Get Q value estimates, action used here is from the replay buffer
-            q2 = critic_model_2([state_batch, action_batch], training=True)
+            q2 = critic_model_2([state_batch, action_batch])
             # Sample actions from the policy for next states
             pi_a, log_pi_a = actor_model(next_state_batch)
 
             # Get Q value estimates from target Q network
-            q1_target = target_critic_1([next_state_batch, pi_a], training=True)
-            q2_target = target_critic_2([next_state_batch, pi_a], training=True)
+            q1_target = target_critic_1([next_state_batch, pi_a])
+            q2_target = target_critic_2([next_state_batch, pi_a])
 
             # Apply the clipped double Q trick
             # Get the minimum Q value of the 2 target networks
@@ -136,8 +155,8 @@ class Buffer:
             # Sample actions from the policy for current states
             pi_a, log_pi_a = actor_model(state_batch)
 
-            q1 = critic_model_1([state_batch, pi_a], training=True)
-            q2 = critic_model_2([state_batch, pi_a], training=True)
+            q1 = critic_model_1([state_batch, pi_a])
+            q2 = critic_model_2([state_batch, pi_a])
 
             soft_q = tf.reduce_mean([q1, q2], axis = 0)
 
@@ -194,7 +213,6 @@ class Actor(Model):
     def __init__(self):
         super().__init__()
         self.action_dim = num_actions
-        self.norm_layer = layers.BatchNormalization()
         self.dense1_layer = layers.Dense(256, activation="relu")
         self.dense2_layer = layers.Dense(256, activation="relu")
         self.mean_layer = layers.Dense(self.action_dim)
@@ -202,7 +220,6 @@ class Actor(Model):
 
     def call(self, state, eval_mode=False):
         # Get mean and standard deviation from the policy network
-        state = self.norm_layer(state, training=not eval_mode)
         a1 = self.dense1_layer(state, training=not eval_mode)
         a2 = self.dense2_layer(a1, training=not eval_mode)
         mu = self.mean_layer(a2, training=not eval_mode)
@@ -237,7 +254,6 @@ class Actor(Model):
 def get_critic():
     # State as input
     state_input = layers.Input(shape=(num_states))
-    state_input = layers.BatchNormalization()(state_input)
     state_out = layers.Dense(128, activation="relu")(state_input)
     # state_out = layers.Dense(32, activation="relu")(state_out)
 
@@ -313,6 +329,7 @@ RO_index = 0
 while t_steps < 1000000:
 
     prev_state = env.reset()
+    prev_state = obs_norm(prev_state)
 
     episodic_reward = 0
 
@@ -327,6 +344,7 @@ while t_steps < 1000000:
 
         # Recieve state and reward from environment.
         state, reward, done, info = env.step(action)
+        state = obs_norm(state)
 
         if done:
             end = 0
@@ -346,6 +364,7 @@ while t_steps < 1000000:
         if t_steps%RO_SIZE == 0:
 
             eval_prev_state = eval_env.reset()
+            eval_prev_state = obs_norm(eval_prev_state)
 
             eval_ep_reward = 0
 
@@ -360,6 +379,7 @@ while t_steps < 1000000:
 
                 # Recieve state and reward from environment.
                 eval_state, eval_reward, eval_done, info = eval_env.step(eval_action)
+                eval_state = obs_norm(eval_state)
 
                 eval_ep_reward += eval_reward
 
