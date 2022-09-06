393d4e967f1a0053675fba941e1565ed0b60ee16
tlaw009 <sxl961@miami.edu>
2022-09-06 10:09:15 -0400

    rbs demo

---
+++ MA2C/sac/rbs_demo.py
@@ -0,0 +1,30 @@
+import sys, os
+from pathlib import Path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import gym
+import scipy.signal
+import time
+from tensorflow.keras import Model
+import matplotlib.pyplot as plt
+import random
+import tensorflow_probability as tfp
+
+from learning.sac import SAC
+from wrappers.robosuite_wrapper import Robosuite_Wrapper
+
+problem = ("Sawyer", "Door", "JOINT_VELOCITY")
+rbs_env = Robosuite_Wrapper("Sawyer", "Door", "JOINT_VELOCITY")
+
+num_states, num_actions, upper_bound, lower_bound = rbs_env.env_specs()
+
+sac1 = SAC(rbs_env, num_states, num_actions, upper_bound, 1000000)
+sac1.load_weights(os.path.dirname(os.path.abspath(__file__))+"/weights")
+
+
+for i in range(10):
+	sac1.eval_rollout(problem, render=True)
