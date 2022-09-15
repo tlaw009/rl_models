5cf008257c217720e8ae98632563606080415f70
tlaw009 <law9706@gmail.com>
2022-09-15 13:52:39 -0400

    incorp var clip

--- MA2C/sac/policies/gaussian_policy.py
+++ MA2C/sac/policies/gaussian_policy.py
@@ -36,7 +36,7 @@ class Actor(Model):
         mu = self.mean_layer(a2)
 
         log_sigma = self.stdev_layer(a2)
-        log_sigma = tf.clip_by_value(log_sigma, -80, 80)
+        log_sigma = tf.clip_by_value(log_sigma, -36, 1)
         sigma = tf.exp(log_sigma)
 
         dist = tfp.distributions.MultivariateNormalDiag(loc=mu, scale_diag=sigma)
